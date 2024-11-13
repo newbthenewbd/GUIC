95578e27db2e6ea38d5cd1fe8fa53a1b17d49a01
Czcibor Bohusz-Dobosz <czbd@o2.pl>
2024-11-13 18:16:57 +0100

    Updates, but bug still not fixed

--- src/window/main/widget/projecttab.cpp
+++ src/window/main/widget/projecttab.cpp
@@ -25,7 +25,7 @@
 #include <opencv2/opencv.hpp>
 //#include <opencv2/world.hpp>
 #define TINYCOLORMAP_WITH_QT5
-#include "../ext/tinycolormap.hpp"
+#include "ext/tinycolormap.hpp"
 #include <vector>
 
 ProjectTab::ProjectTab(QWidget* parent) :
@@ -39,19 +39,19 @@ ui(new Ui::ProjectTab)
 	
 	colormapSelected(0);
 	
-	QStyle* fusion = QStyleFactory::create("Fusion"); //owned by Qt
+    QStyle* fusion = QStyleFactory::create("Fusion"); //pointer ownership belongs to Qt
 	
 	ui->displayConfigButton->setStyle(fusion);
-	ui->displayConfigButton->setIcon(style()->standardIcon(QStyle::SP_FileDialogNewFolder));
+    ui->displayConfigButton->setIcon(style()->standardIcon(QStyle::SP_FileDialogContentsView));
 	
 	for(QObject* object : ui->toolBar->children())
 	{
 		if(object->isWidgetType()) ((QWidget*) object)->setStyle(fusion);
 	}
 	
-	ui->saveProjectButton->setIcon(style()->standardIcon(QStyle::SP_DriveFDIcon));
+    ui->saveProjectButton->setIcon(style()->standardIcon(QStyle::SP_DialogSaveButton));
 	
-	ui->openImagesButton->setIcon(style()->standardIcon(QStyle::SP_DirOpenIcon));
+    ui->openImagesButton->setIcon(style()->standardIcon(QStyle::SP_FileDialogStart));
 	
 	ui->solverButton->setIcon(style()->standardIcon(QStyle::SP_FileDialogDetailedView));
 	solverMenu = new QMenu();
@@ -262,26 +262,26 @@ void ProjectTab::solve()
 					{
 						if(x % 8 == 0 && y % 8 == 0)
 						{
-							poi.push_back(opencorr::POI2D(x, y));
+                            poi.push_back(opencorr::POI2D(x, y));
 						}
 					}
 				}
-			} else poi = data->poi;
+            } else poi = data->poi;
 		}
 		
 		fftcc->setImages(*refImage, *(data->image));
 		fftcc->prepare();
-		fftcc->compute(poi);
+        fftcc->compute(poi);
 		
 		nr->setImages(*refImage, *(data->image));
 		nr->prepare();
-		nr->compute(poi);
+        nr->compute(poi);
 		
 		strain->setApproximation(1); //Cauchy strain
-		strain->prepare(poi);
-		strain->compute(poi);
-		
-		data->poi = poi;
+        strain->prepare(poi);
+        strain->compute(poi);
+
+        data->poi = poi;
 	}
 	
 	ui->statusLabel->setText("Ready");
@@ -386,205 +386,10 @@ void ProjectTab::displayImage()
 			
 			QColor color = ui->colormap->getColor((strain - minStrain) / (maxStrain - minStrain));
 			
-			drawCircle(scene, i.x, i.y, 8.0, color);
+            drawCircle(scene, i.x + displacementX, i.y + displacementY, 8.0, color);
 		}
 	}
 }
-
-#if 0
-	for(QStringList::iterator i = paths.begin(); i != paths.end(); i++)
-	{
-		//Load image into Qt
-		QFileInfo fileInfo(*i);
-		ImageListItem* item = new ImageListItem();
-		item->pixmap = QPixmap(*i);
-		item->setText(fileInfo.fileName()); //TODO check if the name is unique
-		item->setIcon(item->pixmap);
-		ui->listWidget->addItem(item);
-		
-		//Load image into OpenCorr
-		item->image = new opencorr::Image2D(item->pixmap.width(), item->pixmap.height());
-		QImage inImage = item->pixmap.toImage().convertToFormat(QImage::Format_Grayscale8);
-		item->image->cv_mat = cv::Mat(inImage.height(), inImage.width(), CV_8UC1, inImage.bits(), (size_t) inImage.bytesPerLine()); //Taken from [1]; thanks!
-		cv::cv2eigen(item->image->cv_mat, item->image->eg_mat);
-		
-		if(ui->listWidget->count() <= 1) //0 should never happen; set reference image
-		{
-			printf("Setting reference image to %s...\n", qPrintable(fileInfo.fileName()));
-			
-			refImage = item->image;
-		}
-		else
-		{
-			printf("Processing %s...\n", qPrintable(fileInfo.fileName()));
-			
-			if(ui->listWidget->count() == 2) //No previous item to start from
-			{
-				//Set points of interest
-				for(int y = 800; y < 1600; y++)
-				{
-					for(int x = 325; x < 525; x++)
-					{
-						if(x % 8 == 0 && y % 8 == 0)
-						{
-							item->poi.push_back(opencorr::POI2D(x, y));
-						}
-					}
-				}
-				
-				fftcc->setImages(*refImage, *item->image);
-				fftcc->prepare();
-				fftcc->compute(item->poi);
-			}
-			else
-			{
-				item->poi = ((ImageListItem*) ui->listWidget->item(ui->listWidget->count()-2))->poi;
-			}
-			
-			nr->setImages(*refImage, *item->image);
-			nr->prepare();
-			nr->compute(item->poi);
-			
-#ifndef STRAIN
-			for(opencorr::POI2D& i : item->poi)
-			{
-				float displacement = sqrtf(powf(i.deformation.u, 2) + powf(i.deformation.v, 2));
-				
-				if(displacement < minDisplacement) minDisplacement = displacement;
-				if(displacement > maxDisplacement) maxDisplacement = displacement;
-			}
-			printf("min %f max %f\n", minDisplacement, maxDisplacement);
-			for(opencorr::POI2D& i : item->poi)
-			{
-				float displacement = sqrtf(powf(i.deformation.u, 2) + powf(i.deformation.v, 2));
-				
-				if(displacement != 0.0f)
-				{
-					//printf("Displacement at %f %f: %f\n", i.x, i.y, displacement);
-					
-					//unsigned char* color = turbo_srgb_bytes[(unsigned char) ((displacement - minDisplacement) * (255 / (maxDisplacement - minDisplacement)))];
-					QColor color = ui->colormap->getColor((displacement - minDisplacement) / (maxDisplacement - minDisplacement));
-					
-					painter.begin(&item->pixmap);
-					painter.setRenderHint(QPainter::Antialiasing, true);
-					painter.setPen(color); //outline
-					//painter.setBrush(color); //fill
-					
-					//draw arrow
-					painter.drawLine(i.x, i.y, i.x + i.deformation.u, i.y + i.deformation.v);
-					//int angle = atan2(i.x * i.deformation.v - i.y * i.deformation.u, i.x * i.deformation.u + i.y * i.deformation.v) * (180.0 / M_PI) * 16.0;
-					//painter.drawChord(i.x-1, i.y-1, 3, 3, angle-60, 2880+120);
-					painter.drawChord(QRectF(i.x + i.deformation.u - 1.5f, i.y + i.deformation.v - 1.5f, 3.0f, 3.0f), 0, 5760);
-					//painter.drawChord(QRectF(i.x - 1.5f, i.y - 1.5f, 3.0f, 3.0f), 0, 5760);
-					
-					painter.end();
-				}
-			}
-#else
-			//Calculate strains
-			strain->setApproximation(2); //Green strain
-			strain->prepare(item->poi);
-			strain->compute(item->poi);
-			
-			for(opencorr::POI2D& i : item->poi)
-			{
-				//float strain = cbrtf(powf(i.strain.exx, 3) + powf(i.strain.eyy, 3) + powf(i.strain.exy, 3));
-				float strain = i.strain.exx;
-				
-				if(strain < minStrain) minStrain = strain;
-				if(strain > maxStrain) maxStrain = strain;
-			}
-			printf("min %f max %f\n", minStrain, maxStrain);
-			for(opencorr::POI2D& i : item->poi)
-			{
-				//float strain = cbrtf(powf(i.strain.exx, 3) + powf(i.strain.eyy, 3) + powf(i.strain.exy, 3));
-				float strain = i.strain.exx;
-				
-				if(strain != 0.0f)
-				{
-					//printf("Strain at %f %f: %f\n", i.x, i.y, strain);
-					
-					//unsigned char* color = turbo_srgb_bytes[(unsigned char) ((strain - minStrain) * (255 / (maxStrain - minStrain)))];
-					QColor color = ui->colormap->getColor((strain - minStrain) / (maxStrain - minStrain));
-					
-					painter.begin(&item->pixmap);
-					painter.setRenderHint(QPainter::Antialiasing, true);
-					painter.setPen(color);
-					painter.setBrush(color); //fill
-					painter.drawChord(QRectF(i.x, i.y, 8.0, 8.0), 0, 5760);
-					painter.end();
-				}
-			}
-#endif
-		}
-	}
-	
-#if 0
-//#define STRAIN
-	
-#ifdef STRAIN
-	float strainRadius = 20.f;
-	int minNeighbors = 5;
-	opencorr::Strain* strain = new opencorr::Strain(strainRadius, minNeighbors, omp_get_num_procs());
-#endif
-	float minDisplacement = FLT_MAX, maxDisplacement = FLT_MIN;
-	float minStrain = FLT_MAX, maxStrain = FLT_MIN;
-	for(QStringList::iterator i = paths.begin(); i != paths.end(); i++)
-	{
-		//Load image into Qt
-		QFileInfo fileInfo(*i);
-		ImageListItem* item = new ImageListItem();
-		item->pixmap = QPixmap(*i);
-		item->setText(fileInfo.fileName()); //TODO check if the name is unique
-		item->setIcon(item->pixmap);
-		ui->listWidget->addItem(item);
-		
-		//Load image into OpenCorr
-		item->image = new opencorr::Image2D(item->pixmap.width(), item->pixmap.height());
-		QImage inImage = item->pixmap.toImage().convertToFormat(QImage::Format_Grayscale8);
-		item->image->cv_mat = cv::Mat(inImage.height(), inImage.width(), CV_8UC1, inImage.bits(), (size_t) inImage.bytesPerLine()); //Taken from [1]; thanks!
-		cv::cv2eigen(item->image->cv_mat, item->image->eg_mat);
-		
-		//Set points of interest
-		for(int y = 1000; y < 1300; y++)
-		{
-			for(int x = 300; x < 550; x++)
-			{
-				if(x % 8 == 0 && y % 8 == 0)
-				{
-					item->poi.push_back(opencorr::POI2D(opencorr::Point2D(x, y)));
-				}
-			}
-		}
-		
-		if(i == paths.begin())
-		{
-			printf("Setting reference image to %s...\n", qPrintable(fileInfo.fileName()));
-			refImage = item->image;
-		}
-		else //TODO make sure the following images are the same size
-		{
-			fftcc->setImages(*refImage, *item->image);
-			nr->setImages(*refImage, *item->image);
-			
-			printf("Processing %s...\n", qPrintable(fileInfo.fileName()));
-			
-			//Calculate displacements
-			fftcc->prepare();
-			fftcc->compute(item->poi);
-			
-			//nr->prepare();
-			//nr->compute(item->poi);
-
-		}
-	}
-#ifdef STRAIN
-	delete strain;
-#endif
-#endif
-	//delete nr;
-	delete fftcc;
-#endif
 
 bool ProjectTab::eventFilter(QObject* object, QEvent* event)
 {
